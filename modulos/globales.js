d1c04e56eee48b2f6fbec7af7c81720f1ff66492
Francisco Garcia <garciafran@hotmail.com>
2020-04-19 22:38:27 -0500

    globales 2

--- modulos/globales.js
+++ modulos/globales.js
@@ -4,4 +4,21 @@ let intervalo = setInterval(() => {
     if (i === 3) {
         clearInterval(intervalo);
     }
+    i++;
 }, 1000);
+
+setImmediate(() => {
+    console.log('Saludo inmediato');
+});
+
+//requiere para agregar modulos a nuestro proyecto 
+
+//console.log(process);
+//muestra el directorio donde estamos trabajando
+console.log(__dirname);
+//muestra el directorio y el archivo que temenos abierto
+console.log(__filename);
+//es buena practica no usar variables globales
+
+global.miVariable = 'mi variable global';
+console.log(miVariable);
