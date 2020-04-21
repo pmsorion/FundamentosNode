bbba1abe2e7700e7bff80c4d4251cc005752e405
Francisco Garcia <garciafran@hotmail.com>
2020-04-20 20:30:37 -0500

    http

--- modulos/http.js
+++ modulos/http.js
@@ -1,8 +1,41 @@
-const http = requere('http');
+const http = require('http');
+const PUERTO = 3000;
 
-http.createServer((req, res) => {
-    console.log('neuva peticion¡');
-    console.log(req.url);
+http.createServer(router).listen(PUERTO);
 
-    res.end();
-}).lisent(3000);
+function router(req, res) {
+     console.log('nueva peticion¡');
+     console.log(req.url);
+
+     switch (req.url) {
+         case '/hola':
+             res.write('hola, que tal');
+
+             res.end();
+             break;
+        default:
+            res.write('Error 404: no se lo que quieres');
+            res.end();
+     }
+
+    //  res.writeHead(201, { 'Content-Type': 'Funcionamiento ok' })
+
+    // //respuesta para el usuario
+    //  res.write('Hola, estamos usando HTTP de NodeJS')
+
+    //  res.end();
+}
+
+// http.createServer((req, res) =>{
+//     console.log('nueva peticion¡');
+//     console.log(req.url);
+
+//     res.writeHead(201, { 'Content-Type': 'Funcionamiento ok' })
+
+//     //respuesta para el usuario
+//     res.write('Hola, estamos usando HTTP de NodeJS')
+
+//     res.end();
+// }).listen(PUERTO);
+
+// console.log(`Escuchando en el puerto: ${PUERTO}`);
